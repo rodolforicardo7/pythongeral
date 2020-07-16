4a26832eeb1396944f82d00b1251c36960b673e9
Rodolfo Ricardo <rodolforicardo@Mac-mini-5.local>
2020-07-16 16:59:55 -0300

    "Adicionando exemplo WHILE"

---
+++ while.py
@@ -0,0 +1,8 @@
+# Exemplo de WHILE. O comando é executado ENQUANTO.
+
+i = 0 # definindo o valor inicial de i
+while i < 10: # enquanto o i for menor do que 10 faça:
+    print(i) #imprima i
+    i+= 1 # contador. Mesma coisa de i = i + 1. Então, vai rodar o programa,
+    #e volta pro laço até que bata na condição imposta pelo o WHILE.
+    # primeiro é 0, depois é 1 e assim por diante.
