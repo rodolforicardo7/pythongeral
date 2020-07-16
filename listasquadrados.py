255d583de8c592bddaaae0e5a98421a7d4b1ab1f
Rodolfo Ricardo <rodolforicardo@Mac-mini-5.local>
2020-07-16 17:07:57 -0300

    Criando listas com o loop for

---
+++ listasquadrados.py
@@ -0,0 +1,10 @@
+# Exemplos de FOR com listas
+# Montando uma lista com essas condições
+quadrados = [i**2 for i in range(10)]
+quadrados
+
+# Criando uma lista vazia e adicioando os valores
+squares = []
+for n in range(10):
+    squares.append(n**2) #nomedalista.APPEND(valor ao quadrado)
+squares
